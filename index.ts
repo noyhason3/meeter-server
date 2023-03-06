c70f4da31a2a601e4878374a82b5b19a4f0f498f
noy <noyhason3@gmail.com>
2023-03-06 17:00:22 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+// import express from 'express';
+const express = require('express');
+
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+
+app.listen(PORT, () => {
+    console.log('Server is listening on port ' + PORT);
+
+})
