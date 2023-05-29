3c08331216aed6940dde9d27391bdb5af1c08cc0
noy <noyhason3@gmail.com>
2023-05-29 13:00:10 +0300

    cors handle

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const PORT = 3333;
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+
   app.enableCors();
   await app.listen(PORT);
   console.log('Server is listening on port: ' + PORT);
