73771ae22679032f574757655a3c170347f86093
noy <noyhason3@gmail.com>
2023-05-29 13:12:01 +0300

    cors handle

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const PORT = 3333;
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-
+  app.use(cors({
+    origin: 'https://meeter-client.netlify.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
   app.enableCors();
   await app.listen(PORT);
   console.log('Server is listening on port: ' + PORT);
