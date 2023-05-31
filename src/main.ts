f9e094ff87cab4f9c7f927bd09c4d7871bab3de7
noy <noyhason3@gmail.com>
2023-05-31 11:42:18 +0300

    swagger added + firebase

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = 3333;
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Meeter Server')
+    .setDescription('Swagger for meeter api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   const corsOptions: CorsOptions = {
     origin: ['https://meeter-client.netlify.app'], // Add your client's URL here
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Get, Put, Param, Body } from "@nestjs/common";
 import { UserService } from './user.service';
 import { IUser } from "./user.model";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
 
 @Controller('user')
+@ApiTags('user')
 export class UserController {
+
     constructor(private readonly userService: UserService) { }
 
+    @ApiResponse({ status: 200, description: 'Success' })
     @Get()
     async getAllUsers(): Promise<IUser[]> {
         return this.userService.getAllUsers();
